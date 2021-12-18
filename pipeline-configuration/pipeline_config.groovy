cfbce9c9047b42d2d806a4ea8e4d7084ac64d70f
Damon Edstrom <dedstrom@venerated.io>
2021-12-17 18:12:16 -0600

    Added container hooks

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -2,6 +2,7 @@ libraries {
     maven {
         jfrog_user = jfrogCreds_USR
         jfrog_pass = jfrogCreds_PSW
+        image_repo = 'venerated.jfrog.io/default-docker-local/spring-jib-jte'
     }
     generic
 }
